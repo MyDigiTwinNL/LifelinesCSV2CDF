a93355259475ecca698bae8636ce942caa8c9d6b
Hector Cadavid <hector.cadavid@gmail.com>
2023-07-12 10:48:18 +0200

    unit tests and code modularization

---
+++ lifelinescsv_to_icdf/cdfgenerator.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import time
+import resource
+import json
+from typing import Set, Dict, List
+import uuid
+import argparse
+
+def load_val(data_frames:Dict[str,pd.core.frame.DataFrame],file:str,col:str,id:str)->int:
+    return (data_frames[file].loc[id])[col]
+    
+def pseudo_ids()->List[str]:
+    ids_list:List[str] = []
+    for i in range(2,99):
+        ids_list.append(str(uuid.uuid3(uuid.NAMESPACE_DNS,"row"+str(i)))[:15])
+    return ids_list
+
+
+def load_and_index_csv_datafiles(config_file_path:str) -> Dict[str,pd.core.frame.DataFrame]:
+
+    data_frames:Dict[str,pd.core.frame.DataFrame] = {}
+
+    #load transformation configuration file
+    f = open(config_file_path)
+    data = json.load(f)
+    assessment_variables = data.keys();
+    datafiles:Set[set] = set()
+
+    #get the CSV files that are needed for the transformation
+    for assessment_variable in assessment_variables:
+        var_assessment_files = data[assessment_variable]
+        for varversion in var_assessment_files:        
+            datafiles.add(list(varversion.values())[0])
+
+    #create an indexed dataframe for each datafile
+    for file in datafiles:
+        data_frames[file] = pd.read_csv(file);
+        data_frames[file].set_index('ID',inplace=True)
+    
+    return data_frames
+
+
+    #start_load = time.time()
+    #end_load = time.time()
+
+    #print(f"Data indexed in {end_load-start_load} ms")
+
+
+def generate_csd(id:str,config:dict,data_frames:Dict[str,pd.core.frame.DataFrame])->dict:
+    assessment_variables = config.keys()
+    
+    output = {"PROJECT_PSEUDO_ID":{"1A":id}}
+    for assessment_variable in assessment_variables:
+        var_assessments = {}
+        var_assessment_files = config[assessment_variable]
+        for varversion in var_assessment_files:
+            assessment_name = list(varversion.keys())[0]
+            assessment_file = list(varversion.values())[0]                 
+            var_assessments[assessment_name] = load_val(data_frames,assessment_file,assessment_variable,id)    
+        output[assessment_variable]=var_assessments
+    return output    
+
+
+
+
+def main():
+    # Create the command-line argument parser
+    parser = argparse.ArgumentParser(description='Generate JSON output files based on a configuration.')
+
+    # Add the command-line arguments
+    parser.add_argument('ids_file', help='Path to the CSV file with a list of IDs.')
+    parser.add_argument('config_file', help='Path to the JSON configuration file.')
+    parser.add_argument('output_folder', help='Path to the output folder.')
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    ## Generate the output files
+    #generate_output(args.ids_file, args.config_file, args.output_folder)
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_gen.py
+++ tests/test_gen.py
@@ -1,7 +1,47 @@
 import unittest
+import pandas as pd
+from lifelinescsv_to_icdf import cdfgenerator
+from typing import Set, Dict, List
 
 # Define class to test the program
-class TestMathFunctions(unittest.TestCase):
+class CSVFilesToCDF(unittest.TestCase):
 
-    def test_addition(self):        
-        self.assertTrue(False, "unit tests not implemented")
+    def test_addition(self):      
+
+        #Dataframes indexed by PROJECT_PSEUDO_ID
+        file_a = {'PROJECT_PSEUDO_ID':  ['participantA','participantB','participantC'],
+                  'var1':['1'           ,'5'          ,'7'],
+                  'var2':['2'           ,'5'          ,'6']}
+        file_b = {'PROJECT_PSEUDO_ID':  ['participantA','participantB','participantC'],
+                  'var1':['20'          ,'90'          ,'70'],
+                  'var2':['12'          ,'15'          ,'26']}
+        file_c = {'PROJECT_PSEUDO_ID':  ['participantA','participantB','participantC'],
+                  'var1':['100'         ,'500'         ,'700'],
+                  'var2':['200'         ,'500'         ,'600']}
+
+        df_dict:Dict[str,pd.core.frame.DataFrame]=dict()
+
+        df_dict['file_a'] = pd.DataFrame(data=file_a)
+        df_dict['file_a'].set_index('PROJECT_PSEUDO_ID',inplace=True)
+
+        df_dict['file_b'] = pd.DataFrame(data=file_b)
+        df_dict['file_b'].set_index('PROJECT_PSEUDO_ID',inplace=True)
+
+        df_dict['file_c'] = pd.DataFrame(data=file_c)
+        df_dict['file_c'].set_index('PROJECT_PSEUDO_ID',inplace=True)
+
+
+        #Configuration file
+        config = {}
+
+        config['var1'] = [{"1A":'file_a'},{'1B':'file_b'},{'1C':'file_c'}]
+        config['var2'] = [{"3A":'file_a'},{'3B':'file_b'},{'3C':'file_c'}]
+
+        expected_output_participantA = {
+            'PROJECT_PSEUDO_ID':{"1A":'participantA'},
+            'var1':{"1A":"1","1B":"20","1C":"100"},
+            'var2':{"3A":"2","3B":"12","3C":"200"}     
+        }
+  
+                
+        self.assertEqual(cdfgenerator.generate_csd('participantA',config,df_dict),expected_output_participantA,"Not equal")
